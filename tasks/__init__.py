0bcb9ec436b5b3204782e80880a9b9d7ca45e360
Pedro Algarvio <palgarvio@vmware.com>
2023-11-24 16:45:38 +0000

    Switch to python-tools-scripts
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tasks/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from invoke import Collection
-
-from . import containers
-
-ns = Collection()
-ns.add_collection(Collection.from_module(containers, name="containers"), name="containers")
