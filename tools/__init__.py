9287309ccf4adfd5ee75afad03333e01c0afd415
Pedro Algarvio <palgarvio@vmware.com>
2023-11-24 12:36:22 +0000

    Fix containers platforms and metadata (Add actionlint too)
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

---
+++ tools/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+import logging
+
+import ptscripts
+
+ptscripts.register_tools_module("tools.pre_commit")
+
+for name in ("boto3", "botocore", "urllib3"):
+    logging.getLogger(name).setLevel(logging.INFO)
